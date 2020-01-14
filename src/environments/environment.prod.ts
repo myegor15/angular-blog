365fe631b73c9f6be2d541df37e30279731112b4
Yegor Melnichuk <myegor15@gmail.com>
2020-01-14 12:31:18 +0200

    added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBfsxu7aqPvXFHr3hm8P4QccnAWfxwQM1g'
 };
