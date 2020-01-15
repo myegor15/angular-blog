0aab707a392cd2dd55ef622d771162ff266c1ea8
Yegor Melnichuk <myegor15@gmail.com>
2020-01-15 16:27:22 +0200

    add save post to firebase DB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyBfsxu7aqPvXFHr3hm8P4QccnAWfxwQM1g'
+  apiKey: 'AIzaSyBfsxu7aqPvXFHr3hm8P4QccnAWfxwQM1g',
+  fbDbUrl: 'https://angular-blog-61bac.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBfsxu7aqPvXFHr3hm8P4QccnAWfxwQM1g'
+  apiKey: 'AIzaSyBfsxu7aqPvXFHr3hm8P4QccnAWfxwQM1g',
+  fbDbUrl: 'https://angular-blog-61bac.firebaseio.com'
 };
 
 /*
